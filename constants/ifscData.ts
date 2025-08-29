6a84dc9ff6efd2cb84827a85b06f3798042a5d03
FLM-IFSC <228027392+FLM-IFSC@users.noreply.github.com>
2025-08-29 16:28:52 -0300

    rolback

--- constants/ifscData.ts
+++
@@ -1,9 +0,0 @@
-export const IFSC_CAMPUS_LIST: string[] = [
-    "Araranguá", "Caçador", "Canoinhas", "Chapecó", "Criciúma", "Florianópolis", 
-    "Florianópolis-Continente", "Garopaba", "Gaspar", "Itajaí", "Jaraguá do Sul-Centro", 
-    "Jaraguá do Sul-RAU", "Joinville", "Lages", "Palhoça Bilíngue", "Reitoria", 
-    "São Carlos", "São José", "São Lourenço do Oeste", "São Miguel do Oeste", 
-    "Tubarão", "Urupema", "Xanxerê"
-];
-
-export const IFSC_LOGO_BASE64: string = 'iVBORw0KGgoAAAANSUhEUgAAAMgAAABDCAMAAAC2s0taAAAAkFBMVEX////+/v4AAAD8/Pz9/f35+fnz8/P29vb6+vpycnLr6+vY2Njt7e3i4uLs7Ozc3NxgYGDf39+3t7ePj49ISEimpqZfX19bW1tMTEyVlZVVVVWioqJ6enpubm5NTU1dXV05OTlAQEAxMTEhISEbGxsMDAwODg5wcHAvLy8WFhZKSkpLS0uG9kKqAAACq0lEQVR4nO3b63aqMBRAYRRJk6YlBVHELVYprVptvf//H50kCRNSQvJs5z0vF2dg5p0XF1sUAAAAAAAAAAAAAAAAAACYRLSkCylLCsI+xCXRJt+xHBNtEl2SC8lu5hLRJdHlC8mu5/LRVdFlyS/d7KWS0SbRZXslm59LRJtEl+2WjOazyWiTaLJrMvPprCaibRa7Jhu/zGgT0TbLXZONX2Z0iahLdFlOyWb3ZUSbRJeVlWz+NCLaiLZU7ZVs/jYi2oi21OyWbH43ItqItkTtlWz+NiLaiLZU7ZVs/jYi2oi1VM1WsfmbiGgj2vKyvVLN70ZERUTRZWslm39MREREl+2WbH4nIiIiuiwb1Wz+JiIiIroyG9Vs/iYiIiK6LBu/zGgSERGR7pbs/DQiIiK6rKRk87cRERHRZWslm3+NiIiIrlTtlWz+NiIiItrOtlTtlWz+NiIiou1sS9VuyebvIiIi2s62VO2WbH43IiKi7WxL1W7J5u8iIiLaxrJUzVax+ZuIiIhuK8vWSjW/GxER0WXbJZu/jYiIiC4rpWz+NiIiIrolm19mNBkRERHdJpufS0S0iWhLlTslm9+NiCaiLVW7JZvfTYgmom1lW6p2SzZ/FxFNRLtllaVqtmTztxDRRLRLVlqqaivZ/G1ENBFtKdurNlWy+ZuIaiLaUrZXbapE87cQ1US0pWyv2lSJ5m8hqotoS9leqaYaNn+LqCaiLVV7laoazN9C1RPRlpq9SlWN5m8h1hPRlqpdlaoa0PyNiGqi/y+tVlWjaB75m4hmoj9XW6paNaD5m4hmIv653lK1arD5W4gZEX+vN1StGszfQsSIuF49qlY1oPlbiBARV+tHtWoaNP8SERHhqn5UrXbQ/I2IiAj/11+qalL3/hsBAAAAAAAAAAAAAAAAAADA3/kDX55mDk1QIZsAAAAASUVORK5CYII=';
